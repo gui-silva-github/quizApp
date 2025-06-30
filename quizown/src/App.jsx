500275c92283c334b3c733bd456b4a6ea2e44d6d
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2025-06-30 17:42:24 -0300

    App do quiz próprio e estilizações

---
+++ quizown/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header"
+import Quiz from "./components/Quiz"
+
+export default function App(){
+
+  return (
+    <>
+      <Header/>
+      <main>
+        <Quiz/>
+      </main>
+    </>
+  )
+
+}
