707354d71cd406e478ff24dd9098c8e150bd4adc
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2025-06-30 17:41:43 -0300

    Main e questões do quiz próprio

---
+++ quizown/src/questions.js
@@ -0,0 +1,72 @@
+export default [
+  {
+    id: 'q1',
+    text: 'Qual das seguintes definições descreve melhor o React.js?',
+    answers: [
+      'Uma biblioteca para construir interfaces de usuário com a ajuda de código declarativo.',
+      'Uma biblioteca para gerenciar estado em aplicações web.',
+      'Um framework para construir interfaces de usuário com a ajuda de código imperativo.',
+      'Uma biblioteca usada apenas para criar aplicações móveis.',
+    ],
+  },
+  {
+    id: 'q2',
+    text: 'Qual é a finalidade dos hooks do React?',
+    answers: [
+      'Permitir o uso de estado e outras funcionalidades do React em componentes funcionais.',
+      'Criar layouts responsivos em aplicações React.',
+      'Tratar erros dentro da aplicação.',
+      'Parte da biblioteca Redux para gerenciamento de estado global.',
+    ],
+  },
+  {
+    id: 'q3',
+    text: 'Você consegue identificar o que é JSX?',
+    answers: [
+      'Uma extensão do JavaScript que adiciona sintaxe semelhante ao HTML ao JavaScript.',
+      'Uma biblioteca JavaScript para construir interfaces de usuário dinâmicas.',
+      'Uma versão específica do HTML criada explicitamente para o React.',
+      'Uma ferramenta para fazer requisições HTTP em uma aplicação React.',
+    ],
+  },
+  {
+    id: 'q4',
+    text: 'Qual é a forma mais comum de criar um componente no React?',
+    answers: [
+      'Definindo uma função JavaScript que retorna um valor renderizável.',
+      'Definindo uma tag HTML personalizada em JavaScript.',
+      'Criando um arquivo com extensão .jsx.',
+      'Usando a palavra-chave "new" seguida do nome do componente.',
+    ],
+  },
+  {
+    id: 'q5',
+    text: 'O que o termo "React State" implica?',
+    answers: [
+      'Um objeto em um componente que armazena valores e pode causar a renderização do componente ao mudar.',
+      'A fase do ciclo de vida em que um componente React se encontra.',
+      'O status geral de uma aplicação React, incluindo todas as props e componentes.',
+      'Uma biblioteca para gerenciamento de estado global em aplicações React.',
+    ],
+  },
+  {
+    id: 'q6',
+    text: 'Como normalmente se renderiza conteúdo de listas em aplicações React?',
+    answers: [
+      'Usando o método map() para iterar sobre um array de dados e retornar JSX.',
+      'Usando o loop for() para iterar sobre um array de dados e retornar JSX.',
+      'Usando o método forEach() para iterar sobre um array de dados e retornar JSX.',
+      'Usando o método loop() para iterar sobre um array de dados e retornar JSX.',
+    ],
+  },
+  {
+    id: 'q7',
+    text: 'Qual abordagem NÃO pode ser usada para renderizar conteúdo condicionalmente?',
+    answers: [
+      'Usar a sintaxe de template #if.',
+      'Usar o operador ternário.',
+      'Usar o operador &&.',
+      'Usar uma instrução if-else.',
+    ],
+  },
+];
